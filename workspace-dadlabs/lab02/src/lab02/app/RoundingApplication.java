d044d4804759688aa9fe6830170d6cf961112686
Khaizura <khaizura2000khalidan@gmail.com>
2021-10-28 20:20:17 +0800

    Rounding Application
    Output Rounding Application

--- workspace-dadlabs/lab02/src/lab02/app/RoundingApplication.java
+++ workspace-dadlabs/lab02/src/lab02/app/RoundingApplication.java
@@ -1,10 +1,29 @@
 package lab02.app;
 
+import lab02.rounding.RoundingManager;
+
 public class RoundingApplication {
 
 	
 	public static void main(String[] args) {
 		System.out.println("Rounding number to the nearest decimal points");
 
+		
+		int divident = 200;
+		int divisor = 357;
+		RoundingManager roundManager = new RoundingManager();
+		double value = roundManager.calculatePercentage(divident,divisor);
+		
+		System.out.println("Progress from" + divisor + "/"
+				+ divident +":" + value + "%");
+		
+		int decimalPoints = 2;
+		
+		System.out.println("Round to" + decimalPoints + "decimal points:"
+				+ roundManager.roundValue(value,decimalPoints) + "%");
+		
+		decimalPoints = 0;
+		System.out.println("Round to" + decimalPoints + "decimal points:"
+				+ roundManager.roundValue(value, decimalPoints) + "%");
 	}
 }
